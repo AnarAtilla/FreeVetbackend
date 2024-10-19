2d63330cf5e03644cc50cb990b3a4ae05c55df43
AnarAtilla <anar.m.allahverdi@gmail.com>
2024-10-19 02:17:50 +0200

    Ваше сообщение о коммите

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from .models import Profile
 
-
 class RegisterSerializer(serializers.ModelSerializer):
     phone = serializers.CharField(max_length=20)
     photo = serializers.ImageField(required=False, allow_null=True)
@@ -10,11 +9,9 @@ class RegisterSerializer(serializers.ModelSerializer):
         model = Profile
         fields = ['name', 'phone', 'photo']
 
-
 class SMSVerificationSerializer(serializers.Serializer):
     phone_number = serializers.CharField(max_length=15)
     code = serializers.CharField(max_length=6)
-
 
 class LoginSerializer(serializers.Serializer):
     phone_number = serializers.CharField(max_length=15)
--- users/views.py
+++ users/views.py
@@ -5,7 +5,6 @@ from django.conf import settings
 from django.http import HttpResponseRedirect
 from rest_framework import generics, status
 from rest_framework.response import Response
-from rest_framework.authtoken.models import Token
 from .models import Profile
 from .serializers import RegisterSerializer, LoginSerializer, SMSVerificationSerializer
 from .utils import send_sms
@@ -13,15 +12,9 @@ from django.utils import timezone
 from datetime import timedelta
 from rest_framework.generics import CreateAPIView
 
-
-
-"""Redirect after registration and authorization"""
 def custom_login_redirect(request):
     redirect_url = request.session.get('redirect_url', '/default-url')
     return redirect(redirect_url)
-
-
-"""Redirect for creating an API for authorization"""
 
 def google_oauth_redirect(request):
     redirect_url = f"{settings.BASE_URL}/users/social-auth/login/google-oauth2/"
@@ -31,22 +24,23 @@ def facebook_oauth_redirect(request):
     redirect_url = f"{settings.BASE_URL}/users/social-auth/login/facebook/"
     return HttpResponseRedirect(redirect_url)
 
-
-"""Authorization via Twilio"""
-
 class RegisterView(CreateAPIView):
+    serializer_class = RegisterSerializer  # Указываем сериализатор
+
     def post(self, request, *args, **kwargs):
+        if getattr(self, 'swagger_fake_view', False):
+            return None  # Пропускаем выполнение кода при генерации Swagger
+
         serializer = RegisterSerializer(data=request.data)
 
-        # Проверяем валидность входящих данных
         if serializer.is_valid():
             photo = request.FILES.get('photo', None)
             profile = Profile.objects.create(
                 name=serializer.validated_data['name'],
                 phone=serializer.validated_data['phone'],
-                photo=photo  # Устанавливаем фото, если оно передано
+                photo=photo
             )
-            profile.generate_sms_code()  # Генерация кода
+            profile.generate_sms_code()
             send_sms(profile.phone, f"Your code is {profile.sms_code}")
 
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -57,6 +51,9 @@ class LoginView(generics.GenericAPIView):
     serializer_class = LoginSerializer
 
     def post(self, request, *args, **kwargs):
+        if getattr(self, 'swagger_fake_view', False):
+            return None  # Пропускаем выполнение кода при генерации Swagger
+
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         phone_number = serializer.validated_data['phone_number']
@@ -66,18 +63,29 @@ class LoginView(generics.GenericAPIView):
             profile.generate_sms_code()
             send_sms(phone_number, f"Your code is {profile.sms_code}")
 
-            # Создайте токен и верните его
-            token, created = Token.objects.get_or_create(user=profile.user)
-            return Response({"token": token.key}, status=status.HTTP_200_OK)  # Возвратите токен
+            if profile.user is None:
+                user = User.objects.create(username=profile.phone)
+                profile.user = user
+                profile.save()
+
+            refresh = RefreshToken.for_user(profile.user)
+
+            return Response({
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+                "message": "Logged in"
+            }, status=status.HTTP_200_OK)
 
         except Profile.DoesNotExist:
             return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
-
 
 class VerifyCodeView(generics.GenericAPIView):
     serializer_class = SMSVerificationSerializer
 
     def post(self, request, *args, **kwargs):
+        if getattr(self, 'swagger_fake_view', False):
+            return None  # Пропускаем выполнение кода при генерации Swagger
+
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         phone_number = serializer.validated_data['phone_number']
@@ -91,13 +99,11 @@ class VerifyCodeView(generics.GenericAPIView):
 
             profile.last_login_time = timezone.now()
 
-            # Если у профиля нет связанного пользователя, создаём нового
             if profile.user is None:
                 user = User.objects.create(username=profile.phone)
                 profile.user = user
                 profile.save()
 
-            # Генерация JWT токена
             refresh = RefreshToken.for_user(profile.user)
 
             return Response({
@@ -108,5 +114,3 @@ class VerifyCodeView(generics.GenericAPIView):
 
         except Profile.DoesNotExist:
             return Response({"error": "Invalid code"}, status=status.HTTP_400_BAD_REQUEST)
-
-
